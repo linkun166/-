342938208451c309bd85e80873c5ff0a5dfc5af2
akun <13800897+vip1168@user.noreply.gitee.com>
2023-12-14 08:56:23 +0000

    update app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java.
    Signed-off-by: akun <13800897+vip1168@user.noreply.gitee.com>

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://pan.shangui.cc/f/e1qasQ/百度贴吧专用完整版.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
